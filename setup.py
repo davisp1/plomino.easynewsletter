d9ffe80136f2cf4fe151d9c07f97c344cc44935c
Julien Marinescu <jma@makina-corpus.com>
2013-02-20 18:50:03 +0100

    clean ~

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(name='Plomino.EasyNewsletter',
       url='http://svn.plone.org/svn/collective/',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['Plomino'],
+      namespace_packages=['plomino'],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
