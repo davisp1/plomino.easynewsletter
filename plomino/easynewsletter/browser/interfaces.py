45ff8fa360d307892af878e68a918bfec79fe14d
Julien Marinescu <jma@makina-corpus.com>
2013-02-26 15:37:54 +0100

    optimisation

--- plomino/easynewsletter/browser/interfaces.py
+++ plomino/easynewsletter/browser/interfaces.py
@@ -1,12 +1,13 @@
-from zope import schema
-from zope.interface import Interface
 import transaction
 
+from zope import schema
+from zope.interface import Interface
 from zope.i18nmessageid import MessageFactory
-from zope.component import adapts, getUtility, adapter, getAdapters, getUtilitiesFor, getAllUtilitiesRegisteredFor, getGlobalSiteManager, getSiteManager
-from zope.component import getSiteManager
+from zope.component import adapter, getUtilitiesFor, getAllUtilitiesRegisteredFor, getSiteManager
+
 from plone.registry.interfaces import IRecordModifiedEvent
 from Products.EasyNewsletter.interfaces import ISubscriberSource
+
 from plomino.easynewsletter.newsletter import SuscriberSource
 
 _ = MessageFactory('Plomino.EasyNewsletter')
@@ -27,26 +28,18 @@ def registry_edited(itema, itemb):
 
     sm=getSiteManager()
     
-    print_layers(sm)
-    """
-    for x in sm._utility_registrations.keys():
-        if((x[0].__name__=="ISubscriberSource")):
-            del sm._utility_registrations[x]
-            transaction.commit()
-    """
+    #print_layers(sm)
     txts={}
-    #layers = getAllUtilitiesRegisteredFor(ISubscriberSource)
     
     utilities=sm.getUtilitiesFor(ISubscriberSource)
+    utilities2=getAllUtilitiesRegisteredFor(ISubscriberSource)
     
     for cp_item in txt.replace("\r","").split("\n"):
         lst=cp_item.split(":")
         if(len(lst)==2):
             txts[lst[0]]=lst[1]
-            #if len([a for a in layers if a.name == lst[0]])==0:
-            suscribe_utility(sm, lst[1], lst[0])
-
-
+            if len([a for a in utilities if a[0] == lst[0]])==0:
+                suscribe_utility(sm, lst[1], lst[0])
 
     for ut in utilities:
         key=ut[0].__str__()
@@ -54,53 +47,33 @@ def registry_edited(itema, itemb):
             ut2=sm._utility_registrations.get((ISubscriberSource, ut[0]))
             if ut2 is not None:
                 sm.unregisterUtility(ut2, ISubscriberSource, name=ut[0])
-            print(ut)
             sm.utilities.unsubscribe((), ISubscriberSource, ut[1])
-
-
-    #import pdb; pdb.set_trace();
-    """
-    for ut in layers:
+    
+    for ut in utilities2:
         if(ut.name not in txt):
-            ut = layers[-1]
-            name = ut.name
-            #cpt = [a for a in layers if a.name == ut.name]
-            sm.unregisterUtility(ut, ISubscriberSource, name=ut.name)
-            for i in sm.utilities._subscribers[:]:
-                s = i.get(ISubscriberSource, {})
-                for j in s.keys()[:]:
-                    s[j] = tuple([a 
-                                  for a in s[j] 
-                                  if not a is ut])              
-            #sm.utilities.unsubscribe((), ISubscriberSource)
-            #if ISubscriberSource in sm.utilities.__dict__['_provided']:
-            #    del sm.utilities.__dict__['_provided'][ISubscriberSource]
-            del ut
-    """
-    #if ISubscriberSource in sm.utilities.__dict__['_provided']:
-    #    del sm.utilities.__dict__['_provided'][ISubscriberSource]
-        
-    #transaction.commit()        
+            ut2=sm._utility_registrations.get((ISubscriberSource, ut.name))
+            if ut2 is not None:
+                sm.unregisterUtility(ut2, ISubscriberSource, name=ut.name)
+            sm.utilities.unsubscribe((), ISubscriberSource, ut2)
 
     for i in sm.utilities._adapters[:]:
         s = i.get(ISubscriberSource, {})
         for t in s.keys():
             if(t.__str__() not in txt):
                 sm.unregisterUtility(s[t],ISubscriberSource,name=t.__str__())
-                sm.utilities.unsubscribe((), ISubscriberSource, s[t])
-                del s[t]
-
+                try:
+                    sm.utilities.unsubscribe((), ISubscriberSource, s[t])
+                    del s[t]
+                except:
+                    pass
+  
     for x in sm._utility_registrations.keys():
-        if((x[0].__name__=="ISubscriberSource")):
+        if((x[0].__name__=="ISubscriberSource") and ( sm._utility_registrations[x][0].name not in txts.keys())):
             del sm._utility_registrations[x]
-            transaction.commit()
-            
-    sm.utilities.changed(None)
 
     transaction.commit()
     sm._p_jar.sync()
-    print_layers(sm)
-    #sm._p_jar.sync()
+    #print_layers(sm)
 
 def print_layers(sm):
     print "###"
